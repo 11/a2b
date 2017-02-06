a1a7ede729ee398b50f6c2be37dac22a385e332a
11 <drudolph914@gmail.com>
2017-02-06 11:00:20 -0500

    final project

--- run.py
+++ run.py
@@ -1,4 +1,48 @@
-from app import app
+from flask import Flask, render_template, request
+from twilio import twiml
+
+import requests
+import re
+
+
+google_key = 'AIzaSyBEEF4_Wm3qivkCHiQpkEud7oPp7-FwrI0' 
+
+app = Flask(__name__)
+
+@app.route("/")
+def run():
+    return render_template("index.html")
+
+@app.route("/sms", methods=["POST"])
+def text():    
+    phone_num = request.form['From']
+    message_body = request.form['Body']
+    
+    print(message_body)
+
+    mesg_list = message_body.splitlines()
+     
+    origin=mesg_list[0].replace(" ","+")
+    destination=mesg_list[1].replace(" ","+")
+    
+    google_url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+origin+',+NY&destination='+destination+',+NY&key='+google_key+'' 
+
+
+    actual_dirs=""
+
+    directions_json = requests.get(google_url).json()
+    count=1
+    for i in directions_json['routes'][0]['legs'][0]['steps']:
+        actual_dirs+=(str(count)+".)  "+i['html_instructions']).replace("<b>", "").replace("</b>","")+"\n\n"
+        count+=1
+    
+    print("\n"+actual_dirs)
+
+    resp = twiml.Response()
+    resp.message(actual_dirs)
+    return str(resp)
+
+
 
 if __name__ == "__main__":
-	app.run("localhost", port=5000)
+    app.run("localhost", port=8080)
